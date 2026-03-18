2c68ce5617bd21de974033a042c7bc04c005c391
Webml-Community <hw@unhooked.uk>
2026-03-18 00:46:20 +0000

    Delete src directory

--- src/hooks/useLLM.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { LLMContext, type LLMContextValue } from "./LLMContext";
-
-export function useLLM(): LLMContextValue {
-  const ctx = useContext(LLMContext);
-  if (!ctx) throw new Error("useLLM must be used within <LLMProvider>");
-  return ctx;
-}
